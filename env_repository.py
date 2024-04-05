1c62e315549c433c2e60bc5d7312976299464948
AaronMiakito <aaron@miakito.ai>
2024-04-05 17:46:38 +0200

    add weather tool

--- env_repository.py
+++ env_repository.py
@@ -7,5 +7,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AI_MACHINE_URL = os.getenv("AI_MACHINE_URL")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 AI_MACHINE_TIMEOUT = int(os.getenv("AI_MACHINE_TIMEOUT"))
